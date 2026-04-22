e8b7ed3a383e3b5987e3d3bae8caaf1c6f153f5f
rafterminador <ferrafterminador@gmail.com>
2026-04-22 08:02:55 -0600

    feat: adding base project configuration and running project on port 8080

--- src/main/java/com/deportivosshop/DeportivosShopApplication.java
+++ src/main/java/com/deportivosshop/DeportivosShopApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.deportivosshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DeportivosShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DeportivosShopApplication.class, args);
 	}
 
 }
